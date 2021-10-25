c0ecdf5acc41b4feea912be41c7286c310bcb677
ong-wei-hong <ongweihong@protonmail.com>
2021-10-25 11:00:06 +0800

    hopefully fixed cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -3,5 +3,4 @@ Cloudinary.config do |config|
 	config.api_key = ENV['CLOUDINARY_API_KEY']
 	config.api_secret = ENV['CLOUDINARY_API_SECRET']
 	config.secure = true
-	config.cdn_subdomain = true
 end
